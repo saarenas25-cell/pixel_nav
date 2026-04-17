4370528be7e38ce851dab2d592b24699fde05598
elsan <sa.arenas25@ciaf.edu.co>
2026-04-16 19:45:55 -0500

    cuarto commid

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 import pygame
+from players.jugador import Jugador
 
 pygame.init()
-
+reloj = pygame.time.Clock()
 pantalla = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
 pygame.display.set_caption("Pixel Nav") 
+
+player = Jugador(100, 100)
 
 ejecutando = True
 while ejecutando:
@@ -13,5 +16,16 @@ while ejecutando:
         if evento.type == pygame.VIDEORESIZE:
             pantalla = pygame.display.set_mode((evento.w, evento.h), pygame.RESIZABLE)
     pantalla.fill((255, 255, 255))
+#Eventos
+    
+#Lógica (movimiento, etc.)
+    teclas = pygame.key.get_pressed()
+    player.mover(teclas)
+#Dibujar
+    player.dibujar(pantalla)
+#flip()
     pygame.display.flip()
+#tick()
+    reloj.tick(60)
+
 pygame.quit()
---
+++ src/players/jugador.py
@@ -0,0 +1,20 @@
+import pygame
+
+class Jugador:
+    def __init__(self, x, y,):
+        self.x = x
+        self.y = y
+        self.color = (0, 0, 255) 
+
+    def mover(self, teclas):
+        if teclas[pygame.K_LEFT]:
+            self.x -= 5
+        if teclas[pygame.K_RIGHT]:
+            self.x += 5
+        if teclas[pygame.K_UP]:
+            self.y -= 5
+        if teclas[pygame.K_DOWN]:
+            self.y += 5
+
+    def dibujar(self, pantalla):
+        pygame.draw.rect(pantalla, self.color, (self.x, self.y, 50, 50))
