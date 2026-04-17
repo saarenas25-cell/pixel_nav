c0f2e1596aa54fe96e2c051133beb6ac072a6161
elsan <sa.arenas25@ciaf.edu.co>
2026-04-16 20:31:35 -0500

    quinto commid

--- src/main.py
+++ src/main.py
@@ -3,24 +3,29 @@ from players.jugador import Jugador
 
 pygame.init()
 reloj = pygame.time.Clock()
-pantalla = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
+pantalla = pygame.display.set_mode((720, 720), pygame.NOFRAME)
 pygame.display.set_caption("Pixel Nav") 
 
-player = Jugador(100, 100)
+player = Jugador(100, 680)
+print (pantalla)
 
 ejecutando = True
 while ejecutando:
+#Eventos
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             ejecutando = False
+        elif evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_ESCAPE:
+                ejecutando = False
         if evento.type == pygame.VIDEORESIZE:
             pantalla = pygame.display.set_mode((evento.w, evento.h), pygame.RESIZABLE)
     pantalla.fill((255, 255, 255))
-#Eventos
-    
+
 #Lógica (movimiento, etc.)
     teclas = pygame.key.get_pressed()
     player.mover(teclas)
+    print(f"Posición del jugador: ({player.x}, {player.y})")
 #Dibujar
     player.dibujar(pantalla)
 #flip()
--- src/players/jugador.py
+++ src/players/jugador.py
@@ -9,12 +9,16 @@ class Jugador:
     def mover(self, teclas):
         if teclas[pygame.K_LEFT]:
             self.x -= 5
+            if self.x < 0:
+                self.x = 0
         if teclas[pygame.K_RIGHT]:
             self.x += 5
-        if teclas[pygame.K_UP]:
+            if self.x > 670:
+                self.x = 670
+        """if teclas[pygame.K_UP]:
             self.y -= 5
         if teclas[pygame.K_DOWN]:
-            self.y += 5
+            self.y += 5"""
 
     def dibujar(self, pantalla):
         pygame.draw.rect(pantalla, self.color, (self.x, self.y, 50, 50))
